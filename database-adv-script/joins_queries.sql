b28cf2b6f1d95d478ce8a9474451c28c0a0dc390
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-09-12 09:45:55 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,31 @@
+-- INNER JOIN Query
+SELECT 
+    bookings.id AS booking_id,
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
+
+-- LEFT JOIN Query
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.review_text,
+    reviews.rating
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+
+-- FULL OUTER JOIN Query (Standard SQL or MySQL simulation)
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
